574900420322f503cfede6ad475429c5b5034574
Emmanuel Ramirez <eramirez@college.harvard.edu>
2023-05-10 08:38:44 -0400

    Added more content

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
-async function deleteName(ref) {
-    const sentence = $(ref).text();
-    const letters = sentence.split("");
-
-    let i = 0;
-    while (letters.length > 0) {
-        await pause(100);
-        letters.pop();
-        $(ref).html(letters.join(""));
-    }
-}
-
-async function typeName(name, remove, ref) {
-    await pause(500);
-    const letters = name.split("");
-
-    let i = 0;
-    while (i < letters.length) {
-        await pause(100);
-        $(ref).append(letters[i]);
-        i++;
-    }
-
-    if (remove == true) {
-        await pause(2000);
-        deleteName(ref);
-    } else {
-        await pause(700);
-        document.querySelector(".main-wrapper").classList.add("fade-in");
-        executeWordChange();
-        return;
-    }
-
-    await pause(1000);
-    typeName("E-man", false, ".home-name");
-}
-
 function pause(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-$(document).ready(function() {
-    typeName("Emmanuel", true,".home-name");
-});
 
 function executeWordChange() {
     const words = document.querySelector(".animate-word").children,
@@ -69,5 +28,45 @@ function executeWordChange() {
     
     changeWord();
 }
-  
+
+async function deleteName(ref) {
+    const sentence = $(ref).text();
+    const letters = sentence.split("");
+
+    let i = 0;
+    while (letters.length > 0) {
+        await pause(80);
+        letters.pop();
+        $(ref).html(letters.join(""));
+    }
+}
+
+async function typeName(name, remove, ref) {
+    await pause(500);
+    const letters = name.split("");
+
+    let i = 0;
+    while (i < letters.length) {
+        await pause(80);
+        $(ref).append(letters[i]);
+        i++;
+    }
+
+    if (remove == true) {
+        await pause(2000);
+        deleteName(ref);
+    } else {
+        await pause(700);
+        document.querySelector(".main-wrapper").classList.add("fade-in");
+        executeWordChange();
+        return;
+    }
+
+    await pause(1000);
+    typeName("E-man", false, ".home-name");
+}
+
+$(document).ready(function() {
+    typeName("Emmanuel", true,".home-name");
+});
   
